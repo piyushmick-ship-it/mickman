c0f68d224d98daf773665e736cfbd695afc1a40f
piyushmick-ship-it <piyushmick@gmail.com>
2026-07-31 19:50:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ st.title("दुनिया का नया AI असिस्टेंट �
 import streamlit as st
 from groq import Groq
 
-client = Groq(
-    api_key="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"
-)
+client = Groq(api_key="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)
+
 
 if "messages" not in st.session_state:
     st.session_state.messages = [
